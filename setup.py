eaaac5da5c5e7a673ae83d127db4f4431951bbf6
stolpa4 <stolpa413@gmail.com>
2021-12-06 15:44:26 +0300

    Fix version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ elif environ.get('GITHUB_REF'):
 
     version = environ['GITHUB_REF'].replace('refs/tags/v', '')
 else:
-    version = '1.2.2'
+    version = '1.2.2-1'
 
 here = path.abspath(path.dirname(__file__))
 
